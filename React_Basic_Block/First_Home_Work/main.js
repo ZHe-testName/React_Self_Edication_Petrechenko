18df9d40999b46bfc838ef154758dea743d1edd0
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2021-02-16 23:00:57 +0200

    First Hpme Work is done

---
+++ React_Basic_Block/First_Home_Work/main.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const employers = ['Alex', '', 'ludmila', 'Viktor', '', 'oleg', 'iNna', 'Ivan', 'Alex', 'Olga', ' Ann'];
+
+const employersNames = employers.filter(name => name.length > 0)
+                                .map(name => name.toLocaleLowerCase().trim());
+
+const sponsors = {
+    cash: [40000, 5000, 30400, 12000],
+    eu: ['SRL', 'PLO', 'J&K'],
+    rus: ['RusAuto', 'SBO']
+};
+
+function calcCash({cash} = {cash: [0]}){
+    return cash.reduce((acc, num) => num + acc);
+};
+
+const money = calcCash(sponsors);
+
+function makeBusiness([owner, director = 'Victor', cash, emp]){
+    const {eu, rus} = sponsors;
+
+console.log(
+`We have a business. Owner: ${owner}, director: ${director}. Our budget: ${cash}. And our employers: ${emp}
+And we have a sponsors: 
+${eu.concat(rus,  'unexpected sponsor').join(' ')}
+Note. Be careful with ${eu[0]}. It's a huge risk.`
+);
+
+};
+
+makeBusiness(['Sam',, money, employersNames]);
